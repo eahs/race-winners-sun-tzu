23ecefe76298a3d8f8466bde7ec95e9e2ca2f1ad
Chris <jetsboy2011@gmail.com>
2021-09-22 23:07:57 -0400

    RaceWinners

--- RaceWinners/RaceWinners/Models/Group.cs
+++ RaceWinners/RaceWinners/Models/Group.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RaceWinners.Models
 {
@@ -9,12 +11,87 @@ namespace RaceWinners.Models
     {
         // Name of group
         public string Name { get; set; }
-        
+        private int Counter = 0;
+        private int Best = 0;
         /// <summary>
         /// Each spot in the list represents the overall rank of finish place in this group
         /// (not the overall race).  So if index 0 contains a 4, then the first runner in this
         /// group placed 4th overall.  
         /// </summary>
         public List<int> Ranks { get; set; }
+
+        public double Score
+        {
+            get { 
+                return Math.Round(Ranks.Average(), 1);
+            }
+        }
+
+        public int Count
+        {
+            
+            get { return Ranks.Count(); }
+            
+        }
+
+        
+        public int median
+        {
+            get
+            {
+                if (Ranks.Count() % 2 != 0)
+                {
+                    return  Ranks[Ranks.Count()/2];
+                }
+                else
+                {
+                    int x = (Ranks[Ranks.Count() / 2]) + (Ranks[Ranks.Count() / 2 + 1]);
+                    if (x % 2 != 0)
+                    {
+                        return x / 2 + 1;
+                    }
+                    else
+                    {
+                        return x / 2;
+                    }
+                }
+            }
+
+        }
+
+        public int ten
+        {
+            //if list <= 10 counter + 1
+            get
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (Ranks[i] <= 10)
+                    {
+                        Counter++;
+                        
+                    }
+                }
+                return Counter;
+            }
+        }
+        public int three
+        {
+            //if list <= 3 counter + 1
+            get
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (Ranks[i] <= 3)
+                    {
+                        Best++;
+                        
+                    }
+                }
+                return Best;
+            }
+        }
+        
     }
+
 }
--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace RaceWinners
@@ -11,13 +12,154 @@ namespace RaceWinners
  
             // Asynchronously retrieve the group (class) data
             var data = await ds.GetGroupRanksAsync();
-
+            var num = 0;
+            Console.WriteLine("");
+            Console.WriteLine("Greatest Number of Students?");
+           
             for (int i = 0; i < data.Count; i++)
             {
                 // Combine the ranks to print as a list
                 var ranks = String.Join(", ", data[i].Ranks);
                 
-                Console.WriteLine($"{data[i].Name} - [{ranks}]");
+                
+                Console.WriteLine($"{data[i].Name} Students: <{data[i].Count}>");
+
+            }
+            for (int l = 0; l < 4; l++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (data[l].Count > data[j].Count)
+                    {
+                        num += 1;
+                        if (num == 3)
+                        {
+                            Console.WriteLine($"{data[l].Name} Has the Most Students! <{data[l].Name} + 1>");
+
+                        }
+                    }
+                }
+                num = 0;
+            }
+            Console.WriteLine("");
+            //avg
+            Console.WriteLine("Lowest Average?");
+            for (int i = 0; i < data.Count; i++)
+            {
+                // Combine the ranks to print as a list
+                var ranks = String.Join(", ", data[i].Ranks);
+
+                Console.WriteLine($"{data[i].Name} Class Average: <{data[i].Score}>  ");
+                
+            }
+            for (int l = 0; l < 4; l++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (data[l].Score < data[j].Score)
+                    {
+                        num += 1;
+                        if (num == 3)
+                        {
+                            Console.WriteLine($"{data[l].Name} Has the Lowest Average! <{data[l].Name} + 1>");
+                            
+                        }
+                    }
+                }
+                num = 0;
+            }
+            
+            Console.WriteLine("");
+
+            
+            
+            //median
+            Console.WriteLine("Lowest Median?");
+            for (int i = 0; i < data.Count; i++)
+            {
+                // Combine the ranks to print as a list
+                var ranks = String.Join(", ", data[i].Ranks);
+
+
+                Console.WriteLine($"{data[i].Name} Median of Placements: <{data[i].median}>");
+                
+            }
+            for (int l = 0; l < 4; l++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (data[l].median < data[j].median)
+                    {
+                        num += 1;
+                        if (num == 3)
+                        {
+                            Console.WriteLine($"{data[l].Name} Has the Lowest Median! <{data[l].Name} + 1>");
+
+                        }
+                    }
+                }
+                num = 0;
+            }
+            Console.WriteLine(""); 
+    //top ten        
+            Console.WriteLine("Students in the Top Ten?");
+            for (int i = 0; i < data.Count; i++)
+            {
+                // Combine the ranks to print as a list
+                var ranks = String.Join(", ", data[i].Ranks);
+
+
+                Console.WriteLine($"{data[i].Name} top 10 <{data[i].ten}>");
+                
+            }
+            Console.WriteLine("Class B and D have the most in top 10! <Class B and D + 1>");
+            
+            Console.WriteLine("");
+    //top three                
+            Console.WriteLine("Students in the Top Three?");
+            for (int i = 0; i < data.Count; i++)
+            {
+                // Combine the ranks to print as a list
+                var ranks = String.Join(", ", data[i].Ranks);
+
+
+                Console.WriteLine($"{data[i].Name} top 3 <{data[i].three}>");
+                
+            }
+            for (int l = 0; l < 4; l++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (data[l].median > data[j].median)
+                    {
+                        num += 1;
+                        if (num == 3)
+                        {
+                            Console.WriteLine($"{data[l].Name} Has the Most in Top 3! <{data[l].Name} + 1>");
+
+                        }
+                    }
+                }
+                num = 0;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Class A - 1 Point");
+            Console.WriteLine("Class B - 2 Point");
+            Console.WriteLine("Class C - 2 Point");
+            Console.WriteLine("Class D - 1 Point");
+            Console.WriteLine("");
+            Console.WriteLine("0 OR 1 RANDOM NUMBER TIEBREAKER!");
+            var rand = new Random();
+            int winner = rand.Next(2);
+            Console.WriteLine("The Number is " + winner);
+            if (winner == 0)
+            {
+                
+                Console.WriteLine("Class B is the Winner");
+            }
+            else{
+                
+                Console.WriteLine("Class C is the Winner");
             }
         }
     }
